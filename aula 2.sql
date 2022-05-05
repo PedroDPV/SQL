8f5284d652f51e2e08a312f035101b3490a094cd
PedroDPV <103441250+PedroDPV@users.noreply.github.com>
2022-05-05 19:28:26 -0300

    att

--- aula 2.sql
+++ aula 2.sql
@@ -14,3 +14,56 @@ CREATE TABLE aluno
 );
 
 SELECT * FROM aluno;
+
+INSERT INTO aluno (
+	nome,
+	cpf,
+	observacao,
+	idade,
+	dinheiro,
+	altura,
+	ativo,
+	data_nascimento,
+	hora_da_aula,
+	matriculado_em
+) 
+	
+	VALUES (
+		'Pedro',
+		'11182948405',
+		'um texto qualquer grande demais , tipo o lorem ipsum bla bla bla aspodkaspodkaspodkasodaspkodaspoas',
+		35,
+		100.50,
+		1.71,
+		TRUE,
+		'1994-07-24',
+		'17:30:00',
+		'2022-05-03 16:36:00'	
+	)
+	
+SELECT *
+	FROM aluno
+	
+
+UPDATE aluno 
+	SET observacao = 'mayana eu te amo'
+	WHERE id = 6;
+	
+	
+DELETE FROM aluno
+WHERE id = 5;
+
+SELECT nome , idade , hora_da_aula AS hora_aula
+	FROM aluno
+
+SELECT * FROM aluno;
+
+INSERT INTO aluno (nome) VALUES ('João de paula');
+INSERT INTO aluno (nome) VALUES ('Mayana Maranhão');
+INSERT INTO aluno (nome) VALUES ('Alberto Carlos');
+INSERT INTO aluno (nome) VALUES ('Miguel Liberal');
+
+
+UPDATE aluno
+SET nome = 'Pedro Vasconcelos'
+WHERE id = 6;
