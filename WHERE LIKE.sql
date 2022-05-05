5a2e6815e13c5fde1a9d44267c95bc0d54b03d6d
PedroDPV <103441250+PedroDPV@users.noreply.github.com>
2022-05-05 19:29:59 -0300

    att

--- WHERE LIKE.sql
+++ WHERE LIKE.sql
@@ -10,17 +10,17 @@ Agora aprenderemos o caractere % , que substitui todos os caracteres até o espa
 
 SELECT * 
     FROM aluno
- WHERE nome LIKE 'D%';COPIAR CÓDIGO
+ WHERE nome LIKE 'D%';
 Novamente aparecem os resultados do "Diogo" e do "Diego", que são os nomes da nossa tabela que começam dom D. Podemos filtrar também pela última letra ao invés da primeira. Por exemplo, para filtrar os nomes terminados em "s", utilizamos o comando WHERE nome LIKE %s'; . Assim aparecerão os dados do "Vinícius Dias". Outra possibilidade é recuperar todos os nomes que tenham espaço:
 
 SELECT * 
     FROM aluno
- WHERE nome LIKE '% %';COPIAR CÓDIGO
+ WHERE nome LIKE '% %';
 Esse filtro ignora quaisquer nomes estejam antes ou depois do espaço, então os únicos dados que não vão aparecer serão do "Diogo" e do "Diego", porque todos os outros usuários têm nome e sobrenome divididos por espaço. O % também pode ser usado entre textos, como %i%a%.
 
 SELECT * 
     FROM aluno
- WHERE nome LIKE '%i%a%';COPIAR CÓDIGO
+ WHERE nome LIKE '%i%a%';
 Esse comando apresenta os dados que tenham "i**", em alguma parte do texto, seguido por "a", em outra parte do texto. No nosso banco de dados retorna os nomes "Vinícius Dias", devido ao "i" em "Vinícius" e o "a" em "Dias", e o "Nico Steppat", devido ao "i" em "Nico" e o "a" em "Steppa**t".
 
 Até agora aprendemos o filtro de igualdade, de diferença, os filtros com LIKE e NOT LIKE e os caracteres especiais _ , que substitui um caractere qualquer naquela posição específica, e % que substitui múltiplos caracteres no começo, no final ou entre o texto.
